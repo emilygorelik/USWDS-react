37ba1146d902fc0d47bdc9d6697787527a686aa5
Emily Gorelik <emilygorelik2000@gmail.com>
2024-10-18 15:05:57 -0400

    simplify

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import uswdsLogo from '../public/uswds.jpg'
-import emilyLogo from '../public/emily.jfif'
+import uswdsLogo from '/uswds.jpg'
+import emilyLogo from '/emily.jfif'
 import './App.css'
 
 function App() {
